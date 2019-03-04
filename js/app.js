f025ba8e8e94965a1d2a918e84a09c33f5339e56
Brunhildevink <brunhilde_vink@live.nl>
2019-03-04 21:51:04 +0100

    added about me page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+$('#about').on('click', function() {
+    if ($('#about--page').css('display') == 'none') {
+        $('#landing--page').removeClass('show').addClass('hide');
+        $('#about--page').removeClass('hide').addClass('show');
+    }
+});
 
+$('#logo').on('click', function() {
+    if ($('#about--page').css('display') == 'flex') {
+        $('#landing--page').removeClass('hide').addClass('show');
+        $('#about--page').removeClass('show').addClass('hide');
+    }
+});
