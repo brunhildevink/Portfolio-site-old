454b9a56448655d0176693f36cc6fbcfc9402f62
Brunhildevink <brunhilde_vink@live.nl>
2019-03-10 21:59:23 +0100

    Cleaned CSS added rough portfolio

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
 $('#about').on('click', function() {
     if ($('#about--page').css('display') == 'none') {
         $('#landing--page').removeClass('show').addClass('hide');
+        $('#portfolio--page').removeClass('show').addClass('hide');
         $('#about--page').removeClass('hide').addClass('show');
+
+        $('#portfolio').removeClass('active');
+        $('#logo').removeClass('active');
+        $('#about').addClass('active');
     }
 });
 
 $('#logo').on('click', function() {
     if ($('#about--page').css('display') == 'flex') {
-        $('#landing--page').removeClass('hide').addClass('show');
+        $('#portfolio--page').removeClass('show').addClass('hide');
         $('#about--page').removeClass('show').addClass('hide');
+        $('#landing--page').removeClass('hide').addClass('show');
+
+        $('#about').removeClass('active');
+        $('#portfolio').removeClass('active');
+        $('#logo').addClass('active');
+    }
+});
+
+$('#portfolio').on('click', function() {
+    if ($('#portfolio--page').css('display') == 'none') {
+        $('#landing--page').removeClass('show').addClass('hide');
+        $('#about--page').removeClass('show').addClass('hide');
+        $('#portfolio--page').removeClass('hide').addClass('show');
+
+        $('#logo').removeClass('active');
+        $('#about').removeClass('active');
+        $('#portfolio').addClass('active');
     }
 });
